b69bced12dc06c9763601eccc346328f2bc21c7e
Swetha <smilychitty4u@gmail.com>
2024-09-19 12:23:48 +0530

    I created new 1st file

---
+++ 04-shell_conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMEBR-$1
+
+if [ $NUMBER -gt 20 ] #gt,lt,eq,-ne(greater than,less than,equal,not equal)
+then
+   echo "Given Number: $NUMBER is greater than 20"
+else
+   echo "Given Number: $NUMBER is less than 20"
