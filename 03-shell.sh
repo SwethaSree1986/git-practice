33daf84894d8f423987abb37a66357b2afb921d4
Swetha <smilychitty4u@gmail.com>
2024-09-19 12:15:20 +0530

    I created new 1st file

--- 03-shell.sh
+++ 03-shell.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo " All the variables passed to the script: $@"
+echo "All the variables passed to the script: $@"
 echo "Number of variables passed: $#"
 echo "script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "PID of the script executing now: $$" #this is important for interviews
+sleep 100 &
+ech0 "PID of last background command: $!" #this is important for interviews
